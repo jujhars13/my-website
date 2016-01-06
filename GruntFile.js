9590a8c637919294a9375eef79b441d59b34818e
Jujhar Singh <jujhar@jujhar.com>
2016-01-06 20:01:08 +0000

    saving

--- GruntFile.js
+++ GruntFile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'jhsd-history/img/src/',
-                    src: ['*.jpg'],
+                    src: ['*.jpg','*.png','*.gif'],
                     dest: 'jhsd-history/img/dist/'
                 }]
             }
