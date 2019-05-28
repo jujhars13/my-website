df3b3f4ded9b658017b3309c4169ef8bffd7daa8
Jujhar Singh <jujhar@jujhar.com>
2019-05-28 08:58:49 +0100

    ready to test && git pu

--- rotate-iam-keys-install.sh
+++ rotate-iam-keys-install.sh
@@ -7,18 +7,18 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     download="aws-rotate-key-1.0.6-darwin_amd64.zip"
 fi
 
-# download and install aws-rotate-key
+echo "download and install ${download} binary from github" 
 curl -fsSL https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.6/${download} -o "${download}"
 unzip "${download}"
 chmod +x aws-rotate-key
 mv aws-rotate-key /usr/local/bin/aws-rotate-key
 
-# download and install our rotate script
+echo "download and install our rotate script"
 curl -fsSL https://jujhar.com/rotate-iam-keys.sh -o rotate-iam-keys.sh
 chmod +x rotate-iam-keys.sh
 mv /tmp/rotate-iam-keys.sh "${HOME}"
 
-# install into personal crontab
+echo "install into personal crontab"
 touch /var/log/rotate-iam-keys.log && sudo chmod 666 /var/log/rotate-iam-keys.log
 echo "01 12 * * * AWS_SHARED_CREDENTIALS_FILE=${HOME}/.aws/credentials ${HOME}/rotate-iam-keys.sh &>/var/log/rotate-iam-keys.log" | crontab -
 echo "Check /var/log/rotate-iam-keys.log occasionally"
