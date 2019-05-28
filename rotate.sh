b9e3e3028290a630a21fedcc21a04a48ba29aa3d
Jujhar Singh <jujhar@jujhar.com>
2019-05-28 14:57:34 +0100

    ready to test

--- rotate.sh
+++ rotate.sh
@@ -41,6 +41,7 @@ echo "Append into ${local_user} personal crontab"
 # As we're running as sudo we have to take extra steps to install into the calling user's
 # crontab and not root's crontab 
 # @see https://stackoverflow.com/questions/1629605/getting-user-inside-shell-script-when-running-with-sudo
+# NB if you get a "crontab: tmp/tmp.12216: Operation not permitted" type error on your mac it's because you don't have enough permissions
 touch /var/log/rotate-iam-keys.log && chmod 666 /var/log/rotate-iam-keys.log
 (crontab -u "$local_user" -l || true; echo "01 12 * * * AWS_SHARED_CREDENTIALS_FILE=${HOME}/.aws/credentials ${HOME}/rotate-iam-keys.sh > /var/log/rotate-iam-keys.log 2>&1") | \
 crontab -u "$local_user" -
