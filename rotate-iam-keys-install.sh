23c5426a2778b7340feb8fe78338145914b6151c
Jujhar Singh <jujhar@jujhar.com>
2019-05-28 08:58:11 +0100

    ready to test && git pu

--- rotate-iam-keys-install.sh
+++ rotate-iam-keys-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# to run
+# curl -fsSL https://jujhar.com/rotate-iam-keys-install.sh -o rotate-install.sh && sudo bash rotate-install.sh
 
 export download="aws-rotate-key-1.0.6-linux_amd64.zip"
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -6,17 +8,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # download and install aws-rotate-key
-wget https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.6/${download}
+curl -fsSL https://github.com/Fullscreen/aws-rotate-key/releases/download/v1.0.6/${download} -o "${download}"
 unzip "${download}"
 chmod +x aws-rotate-key
 mv aws-rotate-key /usr/local/bin/aws-rotate-key
 
 # download and install our rotate script
+curl -fsSL https://jujhar.com/rotate-iam-keys.sh -o rotate-iam-keys.sh
 chmod +x rotate-iam-keys.sh
-wget https://jujhar.com/rotate-iam-keys.sh
-mv rotate-iam-keys.sh "${HOME}"
+mv /tmp/rotate-iam-keys.sh "${HOME}"
 
-# install crontab
+# install into personal crontab
 touch /var/log/rotate-iam-keys.log && sudo chmod 666 /var/log/rotate-iam-keys.log
 echo "01 12 * * * AWS_SHARED_CREDENTIALS_FILE=${HOME}/.aws/credentials ${HOME}/rotate-iam-keys.sh &>/var/log/rotate-iam-keys.log" | crontab -
 echo "Check /var/log/rotate-iam-keys.log occasionally"
