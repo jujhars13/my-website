770ff12fbfb59edfd04280a3f936d70d01af3838
Jujhar Singh <jujhar@jujhar.com>
2019-05-28 10:22:52 +0100

    ready to test

--- rotate.sh
+++ rotate.sh
@@ -38,8 +38,8 @@ chmod +x rotate-iam-keys.sh
 mv rotate-iam-keys.sh "${HOME}" || true
 
 echo "Append into personal crontab"
-# as we're running as sudo we have to take extra steps to install into the calling user's
-# crontab and not the root crontab 
+# As we're running as sudo we have to take extra steps to install into the calling user's
+# crontab and not root's crontab 
 # @see https://stackoverflow.com/questions/1629605/getting-user-inside-shell-script-when-running-with-sudo
 touch /var/log/rotate-iam-keys.log && sudo chmod 666 /var/log/rotate-iam-keys.log
 (crontab -l ; echo "01 12 * * * AWS_SHARED_CREDENTIALS_FILE=${HOME}/.aws/credentials ${HOME}/rotate-iam-keys.sh &>/var/log/rotate-iam-keys.log") | \
